8b96484c1082c1e7fab9ae05152487c2988c4703
fortyfourh <landkeks.lf@gmail.com>
2023-10-29 14:42:51 -0400

    Webserver now returns index.html that is in the same folder as this assembly program

--- handleRequest.s
+++ handleRequest.s
@@ -1,10 +1,13 @@
 
 .intel_syntax noprefix
 .section .bss
+	.lcomm default_webpage,10000
 .section .ro
 .section .data
 standard_response:
 .ascii "this server is written raw assembly"
+default_webpage_filename:
+.ascii "index.html\0"
 .section .text
 .global readRequest
 
@@ -20,6 +23,26 @@ jmp analyzeAndProcessRequest
 
 analyzeAndProcessRequest:
 # GET / POST / HEAD
+
+# short break, i will try to read index.html and give it back
+mov rax,0x2
+lea rdi,[rip + default_webpage_filename]
+mov rsi, 0
+mov rdx, 0
+syscall
+
+mov r8, rax
+mov rdi,rax
+mov rax,0
+lea rsi,[rip + default_webpage]
+mov rdx, 10000 # 10 KB
+syscall
+
+mov rax,0x3
+mov rdi,r8
+syscall
+
+
 jmp answerRequest
 
 
@@ -27,8 +50,8 @@ answerRequest:
 # answer the request
 mov rax, 1
 mov rdi, [rip + client_sock_fd]
-lea rsi, [rip + standard_response]
-mov rdx, 35
+lea rsi, [rip + default_webpage]
+mov rdx, 10000
 syscall
 
 # Done answering, closing client_socket
